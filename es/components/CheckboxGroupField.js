ecfe3b20360a62303255ab5b40e381b2b1f0a5af
Andres Felipe Martinez Cordero <andrepipemc@gmail.com>
2019-09-07 15:20:47 -0500

    add build functionality

--- es/components/CheckboxGroupField.js
+++ es/components/CheckboxGroupField.js
@@ -9,6 +9,8 @@ var _checkbox = _interopRequireDefault(require("antd/lib/checkbox"));
 
 var _mapError = require("../maps/mapError");
 
+var _immutable = require("immutable");
+
 var _BaseComponent = _interopRequireDefault(require("./BaseComponent"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
@@ -25,6 +27,11 @@ var checkboxGroupMap = (0, _mapError.customMap)(function (mapProps, _ref) {
       onChange = _ref$input.onChange,
       _ref$input$value = _ref$input.value,
       value = _ref$input$value === void 0 ? [] : _ref$input$value;
+
+  if ((0, _immutable.isImmutable)(value)) {
+    value = value.toJS();
+  }
+
   value = (0, _mapError.defaultTo)(value, []);
   return _objectSpread({}, mapProps, {
     onChange: onChange,

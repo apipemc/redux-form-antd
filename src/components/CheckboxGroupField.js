22faf80256665b34ea17685d86db049505e7ee79
Andres Felipe Martinez Cordero <andrepipemc@gmail.com>
2019-09-07 15:12:27 -0500

    fix check groups immutable

--- src/components/CheckboxGroupField.js
+++ src/components/CheckboxGroupField.js
@@ -1,10 +1,14 @@
 import Checkbox from "antd/lib/checkbox";
 import { customMap, defaultTo } from "../maps/mapError";
+import { isImmutable } from 'immutable';
 import createComponent from "./BaseComponent";
 
 const CheckboxGroup = Checkbox.Group;
 
 const checkboxGroupMap = customMap((mapProps, { input: { onChange, value = [] } }) => {
+  if(isImmutable(value)){
+    value = value.toJS();
+  }
   value = defaultTo(value, []);
   return { ...mapProps, onChange, value };
 });
